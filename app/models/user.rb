a6a1c115242ac9173d35f39d43586b49f583e381
hirona96 <d.hiro8611@gmail.com>
2021-12-29 15:13:39 +0900

    userモデル単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true 
+  validates :name, presence: true, length: { maximum: 40 }
   validates :birthday, presence: true
   
   with_options presence: true do
