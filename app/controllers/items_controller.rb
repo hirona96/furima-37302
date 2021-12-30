05105f722b687488358863c1d80381d346dec8cd
hirona96 <d.hiro8611@gmail.com>
2021-12-30 12:21:13 +0900

    itemsコントローラーのindex,new,createアクションを定義。ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,26 @@
 class ItemsController < ApplicationController
+before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 private
   def item_params
-    params.require(:item).permit(,:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :price, :image).merge(user_id: current_user.id)
   end
 
 end
