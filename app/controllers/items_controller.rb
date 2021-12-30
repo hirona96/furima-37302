e28285ba30bb4ce319707ebaaffed594a9a46afe
hirona96 <d.hiro8611@gmail.com>
2021-12-30 16:32:34 +0900

    出品した商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,9 @@ before_action :authenticate_user!, only: [:new]
   end
 
 private
+
   def item_params
-    params.require(:item).permit(:name, :info, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/shipping_fee_status.rb
+++ app/models/shipping_fee_status.rb
@@ -1,4 +1,4 @@
-class Shipping_Fee_Status < ActiveHash::Base
+class ShippingFeeStatus < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '着払い(購入者負担)' },
