e254b485bc7f0388a8009009ea04662016414467
hirona96 <d.hiro8611@gmail.com>
2022-01-06 12:35:32 +0900

    order_addressテーブルにデータを保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
 
   def index
     @order_address = OrderAddress.new
@@ -6,6 +7,20 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+
+  private
+    def order_address_params
+      params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    end
+
 end
