3b2de71f5ae1555dc3637813588d74f4f26f7162
hirona96 <d.hiro8611@gmail.com>
2022-01-07 14:42:59 +0900

    payjpの導入、実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,12 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_address_params[:token],
+        currency: 'jpy'
+      )
       @order_address.save
       redirect_to root_path
     else
@@ -20,7 +26,7 @@ class OrdersController < ApplicationController
 
   private
     def order_address_params
-      params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+      params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
     end
 
 end
