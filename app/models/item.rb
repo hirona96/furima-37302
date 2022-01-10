e9fde32b80a3b35059b24e960136fb5176fd947a
hirona96 <d.hiro8611@gmail.com>
2022-01-10 10:36:14 +0900

    itemモデルのバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,10 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :info
     validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
-    validates :price,
-              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                              message: 'is out of setting range' }
-    validates :image
 
     validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -25,4 +22,8 @@ class Item < ApplicationRecord
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
   end
+
+  validates :price,
+  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                  message: 'is out of setting range' }
 end
